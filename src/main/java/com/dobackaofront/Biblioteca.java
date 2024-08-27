0a889ed992e2d49a4cfc8404e2a43f55a274f234
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2024-08-27 16:13:24 -0300

    update Biblioteca.java

--- src/main/java/com/dobackaofront/Biblioteca.java
+++ src/main/java/com/dobackaofront/Biblioteca.java
@@ -1,4 +1,58 @@
 package com.dobackaofront;
 
+import java.util.ArrayList;
+
 public class Biblioteca {
+    private ArrayList<Livro> livros;
+    private ArrayList<Usuario> usuarios;
+
+    public Biblioteca() {
+        livros = new ArrayList<Livro>();
+        usuarios = new ArrayList<Usuario>();
+    }
+
+    public ArrayList<Livro> getLivros() {
+        return livros;
+    }
+
+    public void setLivros(ArrayList<Livro> livros) {
+        this.livros = livros;
+    }
+
+    public ArrayList<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(ArrayList<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
+
+    public void adicionarLivro(Livro livro) {
+        this.livros.add(livro);
+        System.out.println("Livro" + livro.getTitulo() + " adicionado com sucesso!");
+    }
+
+    public void adicionarUsuario(Usuario usuario) {
+        this.usuarios.add(usuario);
+        System.out.println("Usuario" + usuario.getNome() + " cadastrado com sucesso!");
+
+    }
+
+    public Livro buscarLivro(String titulo) {
+        for (Livro livro : livros) {
+            if (livro.getTitulo().equals(titulo)) {
+                return livro;
+            }
+        }
+        return null;
+    }
+
+    public Usuario buscarUsuario(String nome) {
+        for (Usuario usuario : usuarios) {
+            if (usuario.getNome().equals(nome)) {
+                return usuario;
+            }
+        }
+        return null;
+    }
 }
